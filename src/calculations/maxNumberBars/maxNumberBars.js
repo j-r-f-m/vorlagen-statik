3005a1e5fd9e8c02b5b31441ef5e5e16dc2acdfc
jrfm <moesch.jonas@protonmail.com>
2023-06-14 21:54:53 +0200

    add first implementation of maxNumBars

--- src/calculations/maxNumberBars/maxNumberBars.js
+++ src/calculations/maxNumberBars/maxNumberBars.js
@@ -1,13 +1,12 @@
-function maxNumBars(b, cNomA, cNomI, thetaBü, theta, sMin) {
-  const term1 = 1 - 1 / Math.sqrt(2);
-}
+// ----------------------- Fix -> remove sMin ---------------------------------
 
 /**
  * Berechnet den Biegerollendurchmessers für einen gegebenen Stabdurchmessers
- * @param {number} theta Durchmesser des Stabs in cm
+ * @param {number} theta Durchmesser Längstab in cm
  * @returns number
  */
 function dMin(theta) {
+  // wenn
   if (theta < 2) {
     return 4 * theta;
   } else if (theta >= 2) {
@@ -15,4 +14,85 @@ function dMin(theta) {
   }
 }
 
-export { maxNumBars, dMin };
+/**
+ * Mindestabstand zwischen Längsstäben
+ * @param {number} theta Durchmesser Biegebewehrung
+ * @returns number
+ */
+function sMin(theta) {
+  if (theta < 2) {
+    return 2;
+  } else if (theta >= 2) {
+    return theta;
+  }
+}
+
+/**
+ * Berechne term1
+ * @param {number} b width of beam
+ * @param {number} cNomA Betondeckung außen
+ * @param {number} cNomI Beteckung innen
+ * @param {number} thetaBü Bügel Durchmesser
+ * @returns number
+ */
+function term1(b, cNomA, cNomI, thetaBü) {
+  return b - cNomA - cNomI - 2 * thetaBü;
+}
+
+/**
+ * Berechne term2
+ * @param {number} dMin Biegerollendurchmesser
+ * @returns number
+ */
+function term2(dMin) {
+  const subTerm1 = -(1 - 1 / Math.sqrt(2));
+  const interimResult = subTerm1 * dMin;
+
+  const roundedResult =
+    Math.round((interimResult + Number.EPSILON) * 100) / 100;
+
+  return roundedResult;
+}
+
+/**
+ * Berechne term3
+ * @param {number} theta Durchmesser Längsstab
+ * @param {number} sMin Längsstababstand
+ * @returns number
+ */
+function term3(theta, sMin) {
+  const interimResult = -(1 / Math.sqrt(2)) * theta - theta - sMin;
+  const roundedResult =
+    Math.round((interimResult + Number.EPSILON) * 100) / 100;
+  return roundedResult;
+}
+
+/**
+ * Berechne term4
+ * @param {number} theta Längsstabdurchmesser
+ * @param {number} sMin Längsstabdurchmesser
+ * @returns number
+ */
+function term4(theta, sMin) {
+  return theta + sMin;
+}
+
+function maxNumBars(b, cNomA, cNomI, thetaBü, theta) {
+  const currentDmin = dMin(theta);
+  const currentSmin = sMin(theta);
+
+  const currentTerm1 = term1(b, cNomA, cNomI, thetaBü);
+  const currentTerm2 = term2(currentDmin);
+  const currentTerm3 = term3(theta, currentSmin);
+  const currentTerm4 = term4(theta, currentSmin);
+
+  const interimsResult = Math.floor(
+    (currentTerm1 + currentTerm2 + currentTerm3) / currentTerm4
+  );
+  console.log(interimsResult);
+
+  const nBars = interimsResult + 2;
+  return nBars;
+}
+
+export { maxNumBars, dMin, term1, term2, term3, term4, sMin };
--- src/calculations/maxNumberBars/maxNumberBars.test.jsx
+++ src/calculations/maxNumberBars/maxNumberBars.test.jsx
@@ -1,22 +1,93 @@
-import { screen } from "@testing-library/react";
-import { dMin } from "./maxNumberBars";
+// import { screen } from "@testing-library/react";
+import {
+  dMin,
+  term1,
+  term2,
+  term3,
+  sMin,
+  maxNumBars,
+  term4,
+} from "./maxNumberBars";
 
 import { describe, it, expect } from "vitest";
 
 describe("Biegerollendurchmesser", () => {
-  it("theta_längs = 1.2 cm", () => {
-    const theta_längs = 1.2; // cm
+  it("theta_längs = 1.2 cm < 2.0 cm", () => {
+    const theta_längs = 0.8; // cm
     const biegeRollenDurchmesser = dMin(theta_längs);
 
-    expect(biegeRollenDurchmesser).toBe(4.8);
+    expect(biegeRollenDurchmesser).toBe(3.2);
     //screen.debug();
   });
 
-  it("theta_längs = 2.5 cm", () => {
+  it("theta_längs = 2.5 >= 2.0 cm", () => {
     const theta_längs = 2.5; // cm
     const biegeRollenDurchmesser = dMin(theta_längs);
 
     expect(biegeRollenDurchmesser).toBe(17.5);
     //screen.debug();
+  });
+
+  it("Berechne sMin", () => {
+    const theta = 1; // cm
+    const currentSmin = sMin(theta);
+    expect(currentSmin).toBe(2);
+  });
+
+  it("Berechne Dmin", () => {
+    const thetaBügel = 0.8; // cm
+    const currentDmin = dMin(thetaBügel);
+
+    expect(currentDmin).toBe(3.2);
+  });
+
+  it("Berechne term1", () => {
+    const b = 20; // cm
+    const cNomA = 2.5; // cm
+    const cNomI = 2.5; // cm
+    const thetaBügel = 0.8; // cm
+
+    const currentTerm1 = term1(b, cNomA, cNomI, thetaBügel);
+
+    expect(currentTerm1).toBe(13.4);
+  });
+
+  it("Berechne term2", () => {
+    const thetaBügel = 0.8; // cm
+    const currentDmin = dMin(thetaBügel);
+    const currentTerm2 = term2(currentDmin);
+    expect(currentTerm2).toBe(-0.94);
+  });
+
+  it("Berechne term3", () => {
+    const theta = 1; // cm
+    const currentSmin = sMin(theta);
+    const currentTerm3 = term3(theta, currentSmin);
+    expect(currentTerm3).toBe(-3.71);
+  });
+
+  it("Berechne term4", () => {
+    const theta = 1; //cm
+    const currentSmin = sMin(theta);
+    const currentTerm4 = term4(theta, currentSmin);
+    expect(currentTerm4).toBe(3);
+  });
+
+  /** ------------------- VERIFIKATION -------------------------------------- */
+
+  it("Berechne Anzahl an Eisen1", () => {
+    const b = 20; // cm
+    const currentCnomA = 2.5; // cm
+    const currrentCnomI = 2.5; // cm
+    const currentThetaBügel = 0.8; // cm
+    const currentTheta = 1; // cm
+    const currentMaxNumBars = maxNumBars(
+      b,
+      currentCnomA,
+      currrentCnomI,
+      currentThetaBügel,
+      currentTheta
+    );
+    expect(currentMaxNumBars).toBe(4);
   });
 });
