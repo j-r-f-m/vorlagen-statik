2e16d572c71fd0066259388bf0d872e0d8c0aeae
jrfm <moesch.jonas@protonmail.com>
2023-06-16 15:40:27 +0200

    begin AnchorageLength.jsx

---
+++ src/pages/AnchorageLength/AnchorageLength.jsx
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container";
+
+export function AnchorageLength() {
+  return (
+    <>
+      <Container className="d-flex align-items-center justify-content-center flex-column mt-5">
+        <h1>Verankerungslänge</h1>
+      </Container>
+    </>
+  );
+}
