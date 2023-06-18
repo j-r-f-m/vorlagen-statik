e16639daa3b4923bcbf9c57385e2b6cc38bbad79
jrfm <moesch.jonas@protonmail.com>
2023-06-18 15:47:14 +0200

    add basic AlForm.jsx

---
+++ src/components/ImgAccordion.jsx
@@ -0,0 +1,18 @@
+import Image from "react-bootstrap/Image";
+import Accordion from "react-bootstrap/Accordion";
+
+export function ImgAccordion() {
+  return (
+    <Accordion className="mt-3" style={{ width: "100%" }}>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Verankerungsarten</Accordion.Header>
+        <Accordion.Body className="d-flex justify-content-center">
+          <Image
+            style={{ maxWidth: "50rem" }}
+            src="./imgs/verankerungsarten.png"
+          />
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
